c4bbdb5af78eb428ea2506bc295a4d6c246abc61
Apoorva <vermaapoorva0510@gmail.com>
2023-03-07 19:49:14 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index path="/" element={<Home />} />
         <Route path="/achievements" element={<Achieve />} />
         <Route path="/About" element={<About />} />
         <Route path="/Form" element={<Form />} />
